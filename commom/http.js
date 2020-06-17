935276398355ceb03e8f489fa948f9fe207d49e8
1434706848 <1434706848@qq.com>
2020-06-17 09:10:45 +0800

    修复

--- commom/http.js
+++ commom/http.js
@@ -10,13 +10,12 @@ if (process === 'development') {
 	console.log('开发环境/测试环境')
 	// baseURL = 'https://datacenterapi.szrate.com/api/'; //这里写接口名称就好啦
 	 // baseURL = 'http://api2.szrate.com:8088/api/'; //这里写接口名称就好啦
-	 // baseURL = 'http://192.168.50.111/api/'; //这里写接口名称就好啦
+	 baseURL = 'http://192.168.50.111/api/'; //这里写接口名称就好啦
 	 // baseURL ="http://192.168.50.111/api/";
 	 baseURL ="http://192.168.1.53/api/";
 
 } else {
 	console.log('生产环境/正式环境')
-
 	// baseURL = 'http://192.168.1.115/api/';
 	// baseURL ="http://192.168.1.49/api/";
 	// baseURL = '/formalAPI';
